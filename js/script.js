a07f448ce1a04a76c013bc5624df2538c42f8434
Ashu Sharma <ashusharma0771@gmail.com>
2022-10-21 21:05:59 +0530

    complete

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const qr = document.getElementById("qrcode");
 // Button submit
 const onGenerateSubmit = (e) => {
   e.preventDefault();
+  clearUI();
   const url = document.getElementById("url").value;
   const size = document.getElementById("size").value;
 
@@ -16,6 +17,11 @@ const onGenerateSubmit = (e) => {
     setTimeout(() => {
       hideSpinner();
       generateQRCode(url, size);
+
+      setTimeout(() => {
+        const saveUrl = qr.querySelector("img").src;
+        createSaveBtn(saveUrl);
+      }, 50);
     }, 1000);
   }
 };
@@ -42,5 +48,24 @@ const hideSpinner = () => {
 };
 
 hideSpinner();
+
+const clearUI = () => {
+  qr.innerHTML = "";
+  const saveLink = document.getElementById("save-link");
+  if (saveLink) {
+    saveLink.remove();
+  }
+};
+
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.classList =
+    "bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+  link.href = saveUrl;
+  link.download = "qrcode";
+  link.innerHTML = "Save Image";
+  document.getElementById("generated").appendChild(link);
+};
 
 form.addEventListener("submit", onGenerateSubmit);
